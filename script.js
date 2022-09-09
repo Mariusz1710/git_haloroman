20c336e0943f1d5b29f6b58d8c6e78810f9d28de
Mariusz <karolek@onet.pl>
2022-09-09 16:12:27 +0200

    Beta

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+const cars = new Array("img/mercedes.jpeg","img/redbull.jpeg","img/ferrari.jpeg","img/alpine.jpeg");
 const logoes = document.getElementsByClassName("logo");
 const boxes = document.getElementsByClassName("box");
-
+let inserted = new Array(false,false,false,false);
 
 for(let i=0;i<logoes.length;i++)
 {
 	logoes[i].addEventListener("click",function()
 	{
-		alert(i);					
-		
-		
+		if(!inserted[i])
+		{
+		let image = document.createElement("img");					
+		boxes[i].appendChild(image);
+		image.src = cars[i];
+		inserted[i] = true;
+		}
+		else
+		{
+			boxes[i].removeChild(boxes[i].firstChild);
+			inserted[i] = false;
+		}
 	});	
 }
 
